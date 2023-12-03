96750d14018733cd6b2a63cea78a7294f7a2c510
Jerrified <oyedelejeremiah.ng@gmail.com>
2023-12-03 21:11:24 +0100

    cont'd

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className=' px-6 max-w-6xl mx-auto'>
             <div className='bg-blue-900 mt-8 flex justify-center items-center py-8'>
                 <div className='text-white'>
-                    <p className='text-3xl'>I'm Available For Freelance</p>
+                    <p className='text-3xl'>Im Available For Freelance</p>
                     <p className=''>we provide clean website for your business solution</p>
                 </div>
             </div>
